4fc686ed56bdc6837fe7a9c04bdb6aaae4e59f84
prabhusoftster-dotcom <prabhusoftster@gmail.com>
2026-05-29 19:58:58 +0530

    Create main.tf

---
+++ terraform-ec2/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "vm" {
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
